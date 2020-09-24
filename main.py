4e367cd1848aafa5ece48e8cb663694f599eba26
KwakBro <kwak8947@gmail.com>
2020-09-24 13:48:45 +0900

    need to set up valid save point

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ import tensorflow as tf
 import os
 import random
 import numpy as np
-from keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
-import cv2
-import time
 from keras.models import Sequential
 from keras.layers import Dropout, Activation, Dense
 from keras.layers import Flatten, Convolution2D, MaxPooling2D
-from keras.models import load_model
 import cv2
 
 categoris = ["door", "empty", "window"]
@@ -27,8 +23,8 @@ X_V = []
 Y_V = []
 T = []
 for index, cate in enumerate(categoris):
-    #label = [0 for i in range(3)]
-    #label[index] = 1
+    # label = [0 for i in range(3)]
+    # label[index] = 1
     image_dir = path + cate + '\\'
     valid_dir = path + cate + '\\'
 
@@ -66,36 +62,32 @@ Y = [n[1] for n in temp]
 
 X = np.array(X)
 X = X.reshape(X.shape[0], img_h, img_w, 1)
-#Y = np.array(Y)
+# Y = np.array(Y)
 Y = tf.keras.utils.to_categorical(Y, num_classes=3)
 
 X_V = np.array(X_V)
 X_V = X_V.reshape(X_V.shape[0], img_h, img_w, 1)
-#Y_V = np.array(Y_V)
+# Y_V = np.array(Y_V)
 Y_V = tf.keras.utils.to_categorical(Y_V, num_classes=3)
 
-
-print(" and,, shape of picture = ")
-print(X.shape)
-
 model = Sequential()
-model.add(Convolution2D(16, (3, 3),  activation='relu',
+model.add(Convolution2D(20, (3, 3),  activation='relu',
                         input_shape=(img_w, img_h, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
+# model.add(Dropout(0.25))
 
-model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(Convolution2D(30, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
+# model.add(Dropout(0.25))
 
-model.add(Convolution2D(64, (3, 3)))
+model.add(Convolution2D(30, (3, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
+# model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 
-#model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
 model.add(Dense(3, activation='softmax'))
 
 model.compile(loss=tf.keras.losses.binary_crossentropy,
@@ -103,6 +95,7 @@ model.compile(loss=tf.keras.losses.binary_crossentropy,
 model.fit(X, Y, batch_size=20, epochs=15, verbose=2, validation_data=(X_V, Y_V))
 
 model.save('RESULT_CALSSIFIRE.h5')
+model.save(r'C:\Users\kwak8\PycharmProjects\Version 1.0\RESULT_CALSSIFIRE.h5')
 model.summary()
 
 name = []
@@ -115,11 +108,8 @@ for top, dir, f in os.walk(path_test):
         name.append(filename)
         img = cv2.resize(img, None, fx=img_w/img.shape[1], fy=img_h/img.shape[0])
 
-        '''
-        cv2.imshow('ori', img)
-        cv2.waitKey()
-        '''
         T.append(img / 256)
+
 T = np.array(T)
 T = T.reshape(T.shape[0], img_h, img_w, 1)
 pre = model.predict_classes(T)
